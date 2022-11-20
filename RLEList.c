8860863cf306d44df49e9422b82d049889d018a3
omermeushar <omermeushar@campus.technion.ac.il>
2022-11-20 22:47:15 +0200

    20/11 - started project

--- RLEList.c
+++ RLEList.c
@@ -2,6 +2,22 @@
 
 struct RLEList_t{
     //TODO: implement
+    RLENode first;
+    RLENode last;
+    //int size
+    //numLetters
 };
 
+/***
+ * typedef enum {
+    RLE_LIST_SUCCESS,
+    RLE_LIST_OUT_OF_MEMORY,
+    RLE_LIST_NULL_ARGUMENT,
+    RLE_LIST_INDEX_OUT_OF_BOUNDS,
+    RLE_LIST_ERROR
+} RLEListResult;
+ */
+
+
+/*** Function Implement ***/
 //implement the functions here
